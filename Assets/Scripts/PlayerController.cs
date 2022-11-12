1b6bfa59f2ba95f12d91fb2af8c030eff236d901
MasterZuse <drostandrew@gmail.com>
2022-11-11 22:10:29 -0600

    Basic Animation Controlling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,12 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float speed = 10.0f;
 
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +28,8 @@ public class PlayerController : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
 
         transform.Translate(horizontal, vertical, 0);
+
+        animator.SetFloat("Speed", Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical)));
     }
 
     private void Fire()
