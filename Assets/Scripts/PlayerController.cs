77ad3efda5985c6a870250c8527d1b514e075260
MasterZuse <drostandrew@gmail.com>
2022-11-11 20:27:56 -0600

    Basic Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] float speed = 10.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(horizontal, vertical, 0);
     }
 }
