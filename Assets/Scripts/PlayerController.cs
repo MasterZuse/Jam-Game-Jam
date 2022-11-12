2becffb8df7b093af5350a172eaade7dace5af21
MasterZuse <55260245+MasterZuse@users.noreply.github.com>
2022-11-12 11:56:29 -0600

    Basic Animations Complete
    Still needs polishing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,33 +7,70 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float speed = 10.0f;
 
+    bool canMove = true;
+    bool bagOpen = false;
+
     Animator animator;
+    SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        SpaceButton();
     }
 
 
-    private void Move()
-    {
+    private void Move() {
+        if (!canMove) {
+            return;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
         float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
 
         transform.Translate(horizontal, vertical, 0);
 
-        animator.SetFloat("Speed", Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical)));
+        if (horizontal < 0) { 
+            sprite.flipX = true;
+        } else if (horizontal > 0) {
+            sprite.flipX = false;
+        }
+
+        float greatestSpeed = Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical));
+        if (greatestSpeed > 0) {
+            bagOpen = false;
+        }
+
+        animator.SetFloat("Speed", greatestSpeed);
+    }
+
+    private void SpaceButton() {
+        if (Input.GetAxisRaw("Jump") > 0) {
+            if (!bagOpen) {
+                canMove = false;
+                animator.SetBool("Unsling", true);
+                animator.SetFloat("Speed", 0);
+            }
+        }
     }
 
     private void Fire()
     {
-        //Shoot the jam
+        
     }
+
+    private void FinishUnsling() {
+        bagOpen = true;
+        canMove = true;
+        animator.SetBool("Unsling", false);
+    }
+
 }
