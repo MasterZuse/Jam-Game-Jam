6bf5df50c0592288f016fa4a9bf77cf5f6bff9d8
MasterZuse <drostandrew@gmail.com>
2022-11-12 19:03:48 -0600

    Jam Splat

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,29 +6,27 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] int damage = 10;
-    [SerializeField] float radius = 2f;
     [SerializeField] float speed = 250f;
 
+    bool active = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
+        if (!active) {
+            return;
+        }
         string tag = collision.gameObject.tag;
         if (tag == "Player") {
             return;
         } else if (tag == "Enemy") {
             collision.GetComponent<Enemy>().TakeDamage(damage);
         }
+        active = false;
+        GetComponent<Animator>().SetBool("Splat", true);
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    private void Finish() {
         Destroy(gameObject);
     }
 
