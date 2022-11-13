132f06098885c290799dc37adad35571a9bffd16
MasterZuse <drostandrew@gmail.com>
2022-11-13 17:46:47 -0600

    boss stuff

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -16,17 +16,20 @@ public class BossController : MonoBehaviour
     int nextCoolDown = 0;
 
     Animator animator;
+    GameObject playerObj;
 
-    // Start is called before the first frame update
     void Start() {
         animator = GetComponent<Animator>();
         currentCooldown = longCooldown * 2;
+        playerObj = GameObject.FindGameObjectsWithTag("Player")[0];
     }
 
-    // Update is called once per frame
     void Update() {
         if (fightActive) {
             currentCooldown = Mathf.Max(currentCooldown - Time.deltaTime, 0);
+            if (currentCooldown <= 0) {
+                animator.SetBool("Throw", true);
+            }
         }
     }
 
@@ -43,5 +46,50 @@ public class BossController : MonoBehaviour
 
     private void Goodbye() {
         Destroy(gameObject);
+    }
+
+    public void Activate() {
+        fightActive = true;
+    }
+
+    private void Attack() {
+        animator.SetBool("Throw", false);
+        int rand = (int) Mathf.Floor(Random.Range(0, 3));
+        if (rand == 0) {
+            BasicAttack();
+        } else if (rand == 1) {
+            CircleAttack();
+        } else {
+            CircleAttack2();
+        }
+        nextCoolDown = (nextCoolDown + 1) % 3;
+        if (nextCoolDown == 0) {
+            currentCooldown = longCooldown;
+        } else {
+            currentCooldown = shortCooldown;
+        }
+    }
+
+    private void LaunchProjectile(float angle) {
+        Projectile p = Instantiate(projectile, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+        p.Shoot();
+    }
+
+    private void BasicAttack() {
+        var dir = playerObj.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        LaunchProjectile(angle);
+    }
+
+    private void CircleAttack() {
+        for (int i = 0; i < 8; i++) {
+            LaunchProjectile(i * 45);
+        }
+    }
+
+    private void CircleAttack2() {
+        for (int i = 0; i < 8; i++) {
+            LaunchProjectile(i * 45 + 22.5f);
+        }
     }
 }
--- Assets/Scripts/SongPlayer.cs
+++ Assets/Scripts/SongPlayer.cs
@@ -8,10 +8,17 @@ public class SongPlayer : MonoBehaviour
     [SerializeField] float loopStart;
     [SerializeField] float loopEnd;
     [SerializeField] AudioSource source;
+    [SerializeField] AudioClip secondClip;
 
     void Update() {
         if (source.time > loopEnd) {
             source.time -= loopEnd - loopStart;
         }
+    }
+
+    public void ChangeSong() {
+        source.Stop();
+        source.clip = secondClip;
+        source.Play();
     }
 }
