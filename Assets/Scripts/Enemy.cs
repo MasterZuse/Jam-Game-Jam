91bbdf1788a93723c6575f91b2a3f8e85ee0a818
MasterZuse <drostandrew@gmail.com>
2022-11-12 19:14:08 -0600

    Enemy Patrol

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,23 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
+
+
 public class Enemy : MonoBehaviour
 {
 
     [SerializeField] int health = 50;
     [SerializeField] float speed = 2f;
+    [SerializeField] Direction patrolDirection;
+    [SerializeField] float patrolDistance;
+    float distanceLeft;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        distanceLeft = patrolDistance;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Patrol();
+    }
+
+    public void Patrol() {
+        float movement = speed * Time.deltaTime;
+        if (patrolDirection == Direction.LEFT) {
+            transform.Translate(-movement, 0, 0);
+            distanceLeft -= movement;
+            if (distanceLeft <= 0) {
+                distanceLeft = patrolDistance;
+                patrolDirection = Direction.RIGHT;
+            }
+        } else if (patrolDirection == Direction.RIGHT) {
+            transform.Translate(movement, 0, 0);
+            distanceLeft -= movement;
+            if (distanceLeft <= 0) {
+                distanceLeft = patrolDistance;
+                patrolDirection = Direction.LEFT;
+            }
+        } else if (patrolDirection == Direction.UP) {
+            transform.Translate(0, movement, 0);
+            distanceLeft -= movement;
+            if (distanceLeft <= 0) {
+                distanceLeft = patrolDistance;
+                patrolDirection = Direction.DOWN;
+            }
+        } else if (patrolDirection == Direction.DOWN) {
+            transform.Translate(0, -movement, 0);
+            distanceLeft -= movement;
+            if (distanceLeft <= 0) {
+                distanceLeft = patrolDistance;
+                patrolDirection = Direction.UP;
+            }
+        }
     }
 
     public void TakeDamage(int amount) {
